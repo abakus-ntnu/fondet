4a718ddcee333fb2b64232d83a31ffdc47894ec4
Collatty <hakoncb@gmail.com>
2018-10-25 00:14:44 +0200

    Components for iframes from nordnet

---
+++ src/positions/Positions.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Iframe from "react-iframe";
+
+const Positions = () => {
+  return (
+    <Iframe
+      url="https://www.shareville.no/widget/portfolio/324475/positions"
+      frameborder="0"
+      allowtransparency="true"
+      scrolling="yes"
+    />
+  );
+};
+
+export default Positions;
