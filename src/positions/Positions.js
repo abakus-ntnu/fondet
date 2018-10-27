c2c70a79a392f318c2272fdb7083099d2fa0bef0
Collatty <hakoncb@gmail.com>
2018-10-27 04:46:10 +0200

    nada

--- src/positions/Positions.js
+++ src/positions/Positions.js
@@ -1,12 +1,43 @@
-import React from "react";
-import Iframe from "react-iframe";
+import axios from "axios";
+import React, { Component } from "react";
+import { VictoryPie } from "victory";
 
-const Positions = () => {
-  return (
-    <div className="positions">
-      <Iframe url="https://www.shareville.no/widget/portfolio/324475/positions?limit=10" />
-    </div>
-  );
-};
+class Positions extends Component {
+  constructor() {
+    super();
+    this.state = { res: [], data: [] };
+  }
+  //Getting info from nordnet-api and cleaning for victorypie
+  async componentDidMount() {
+    const res = await axios.get("/api/v1/portfolios/324475/positions");
+    this.setState({ res: res.data });
+    console.log(res.data.length);
+    console.log(res.data[0].instrument.name);
+    for (var i = 0; i < res.data.length; i++) {
+      this.setState({
+        data: [
+          ...this.state.data,
+          { name: res.data[i].instrument.name, percent: res.data[i].percent }
+        ]
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <VictoryPie
+          data={this.state.data}
+          x="name"
+          y="percent"
+          radius={100}
+          style={{
+            labels: { fill: "black", fontSize: 5, fontWeight: "bold" }
+          }}
+        />
+      </div>
+    );
+  }
+}
 
 export default Positions;
