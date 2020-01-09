893fc2321c7278718531b9142b3a68242529fc6d
eivinhoy <eivind@hoydal.net>
2020-01-09 15:26:21 +0100

    updated text in history and philosophy

--- client/src/history/History.js
+++ client/src/history/History.js
@@ -4,17 +4,13 @@ import React, { Component } from "react";
 class History extends Component {
   render() {
     return <div className="history">
-        <h2>Historie</h2>
+        <h2>Fondets historie</h2>
         <p>
-            Fondet ble opprettet under generalforsamlingen i 2013 fordi Hovedstyret ikke hadde noen retningslinjer når det gjaldt disponeringen av Abakus’ økonomiske midler. 
-            Det er altså i hovedsak til for at Hovedstyret ikke skal kunne sløse bort alle pengene våre. Kjekt å vite, siden Abakus begynner å få en del overskudd. 
-            Tanken var at det var bedre å la noen rutinerte (les: for det meste gamle, grå og kjipe) abakuler ta avgjørelsene dersom det skulle oppstå store økonomiske spørsmål.
+            Fondet ble opprettet under generalforsamlingen i 2013. <i>Abakus’ fonds formål er å gi Abakus en økonomisk trygghet og sørge for at Abakus sitt økonomiske overskudd fordeles rettferdig på nåværende og fremtidige medlemmer av Abakus.</i> 
+            Bakgrunnen var at Abakus ikke hadde noen retningslinjer når det gjaldt disponeringen av Abakus’ økonomiske midler. 
+            Fondet eksisterer i all hovedsak til for at Hovedstyret ikke skal forvalte alle pengene våre. Tanken var at det var bedre å la noen rutinerte (les: for det meste gamle, grå og kjipe) abakuler ta avgjørelsene dersom det skulle oppstå store økonomiske spørsmål.
             I tillegg ønsket man å gjøre Abakus’ økonomiske midler mer tilgjengelige for abakuler som setter i gang gode initiativ og prosjekter. 
-            Eksempler på tidligere godkjente søknader har vært nytt draktsett til Datakameratene og støtte til å pusse opp LaBamba. </p>
-        <p>
-            Fondets disponering av midler har vært oppe til diskusjon flere ganger. Blant annet da vi under generalforsamlingen i 2017 diskuterte om Abakus burde kjøpe hytte eller ei. 
-            I statuttene til Fondet står det at pengene skal komme nåværende og fremtidige medlemmer av Abakus til gode. 
-            Det har følgelig blitt diskutert om det bør fokuseres mer på de nåværende studentene enn det som er blitt gjort til nå.
+            Eksempler på tidligere godkjente søknader har vært nytt draktsett til Datakameratene og støtte til å pusse opp LaBamba. Fondets disponering av midler har vært oppe til diskusjon flere ganger. Blant annet da vi under generalforsamlingen i 2017 diskuterte om Abakus burde kjøpe hytte.
         </p>
         </div>;
   }
--- client/src/philosophy/Philosophy.js
+++ client/src/philosophy/Philosophy.js
@@ -4,13 +4,14 @@ import React, { Component } from "react";
 class Philosophy extends Component {
   render() {
     return <div className="philosophy">
-        <h2>Filiosofi</h2>
-        <p>Så hvordan fungerer egentlig Fondet? Slik det er nå styrer Fondet en portefølje og en høyrentekonto. 
-            Pengene på kontoen blir investert gradvis – litt hver måned – i åtte ulike fond, spredd over hele verden. 
+        <h2>Slik er fondet investert</h2>
+        <p>
+           Fondet består i dag av en fondsportefølje i Nordnet og en høyrentekonto i DNB. Frem til sommeren 2020 blir pengene på kontoen investert gradvis – litt hver måned – i åtte ulike fond, spredd over hele verden. 
             For å bestemme hvor mye som blir investert hver måned, tas value averaging i bruk. 
-            Uten å gå for mye inn på finanspensum, vil det si at man fordeler ut risikoen ved at man ikke investerer alt overskuddet på ett tidspunkt, noe som reduserer risikoen for at det går skeis. 
-            Denne metoden blir hovedsakelig brukt fordi Fondstyret ikke har peiling på det de driver med, i likhet med resten av aksjemarkedene. 
-            Fondstyremedlemmene har heldigvis også ulike retningslinjer for hvordan pengene på høyrentekontoen skal disponeres.</p>
+            Value averaging vil si at man fordeler ut risikoen ved å investere en månedlig sum som blir beregnet ut fra et avkastningsmål på fem prosent. Ligger vi foran målet blir en mindre sum investert, og ligger vi bak blir en større sum investert. 
+            Dette reduserer risikoen for at vi taper penger ved å investere i en 'topp', men det reduserer også sannsynligheten for at vi tjener penger på å investere i en 'bunn'. 
+            Denne metoden blir hovedsakelig brukt fordi vi i Fondstyret ikke tror vi kan slå markedet over tid, og dermed foretrekker forutsigbarhet. 
+        </p>
         </div>;
   }
 }
