f90c2f0d6506844c51a7217bcaf7888794680100
sjur <sjure@stud.ntnu.no>
2020-01-17 14:10:57 +0100

    styling of font and linebreaks in text

--- client/src/history/History.js
+++ client/src/history/History.js
@@ -6,8 +6,10 @@ class History extends Component {
     return <div className="history">
         <h2>Fondets historie</h2>
         <p>
-            Fondet ble opprettet den 11. mars 2013 under generalforsamlingen til Abakus. <i>Abakus’ fonds formål er å gi Abakus en økonomisk trygghet og sørge for at Abakus sitt økonomiske overskudd fordeles rettferdig på nåværende og fremtidige medlemmer av Abakus.</i> 
-            Bakgrunnen var at Abakus ikke hadde noen retningslinjer når det gjaldt disponeringen av Abakus’ økonomiske midler. 
+            Fondet ble opprettet den 11. mars 2013 under generalforsamlingen til Abakus. <em>Abakus’ fonds formål er å gi Abakus en økonomisk trygghet og sørge for at Abakus sitt økonomiske overskudd fordeles rettferdig på nåværende og fremtidige medlemmer av Abakus.</em>
+            &nbsp;Bakgrunnen var at Abakus ikke hadde noen retningslinjer når det gjaldt disponeringen av Abakus’ økonomiske midler.
+        </p>
+        <p>
             Fondet eksisterer i all hovedsak til for at Hovedstyret ikke skal forvalte alle pengene våre. Tanken var at det var bedre å la noen rutinerte (les: for det meste gamle, grå og kjipe) abakuler ta avgjørelsene dersom det skulle oppstå store økonomiske spørsmål.
             I tillegg ønsket man å gjøre Abakus’ økonomiske midler mer tilgjengelige for abakuler som setter i gang gode initiativ og prosjekter. 
             Eksempler på tidligere godkjente søknader har vært nytt draktsett til Datakameratene og støtte til å pusse opp LaBamba. Fondets disponering av midler har vært oppe til diskusjon flere ganger. Blant annet da vi under generalforsamlingen i 2017 diskuterte om Abakus burde kjøpe hytte.
