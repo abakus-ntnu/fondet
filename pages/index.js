b403dda227a3308f9d66923e43136be470b18f32
Peder Smith <smith.peder@gmail.com>
2020-02-08 16:10:04 +0100

    Move root App to pages folder

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./App.css";
+import HeaderWrap from "../header/HeaderWrap.js";
+import Positions from "../positions/Positions.js";
+import Development from "../development/Development.js";
+import Numbers from "../numbers/Numbers.js";
+import Philosophy from "../philosophy/Philosophy.js";
+import History from "../history/History.js";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app" >
+          <HeaderWrap/>
+        <Development />
+        <Positions />
+        <Philosophy />
+        <History />
+      </div>
+    );
+  }
+}
+
+export default App;
