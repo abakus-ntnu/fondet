a03cb40955918998394d84ac2300aa411a17e341
Jonatan Norbye <jonatan@norbye.com>
2025-01-23 20:42:54 +0100

    Implement sanity

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,22 @@
+import { visionTool } from '@sanity/vision';
+import { defineConfig } from 'sanity';
+import { schemaTypes } from './schemas';
+import { structureTool } from 'sanity/structure';
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '';
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || '';
+
+export default defineConfig({
+  basePath: '/studio',
+  name: 'default',
+  title: 'fondet.abakus.no',
+
+  projectId,
+  dataset,
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
